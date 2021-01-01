b52170aef0046ef47c000abef8377a63ffac386d
易成杰 <626659321@qq.com>
2021-01-01 11:36:30 +0800

    初始版本sso准备改造项目可用sso

---
+++ auth-server/src/main/java/com/yicj/study/auth/config/OAuth2WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.yicj.study.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class OAuth2WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("guest").password("guest").authorities("ROLE_GUEST").and()
+                .withUser("admin").password("admin").authorities("ROLE_GUEST","ROLE_ADMIN") ;
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance() ;
+    }
+}
---
+++ zuul-server/src/main/java/com/yicj/study/zuul/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.yicj.study.zuul.config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+//@EnableWebSecurity 这里可以不用添加
+@Configuration
+@EnableOAuth2Sso
+public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/login","/resource/**","/uua/**").permitAll()
+            .anyRequest().authenticated().and()
+            .csrf().disable() ;
+    }
+}
